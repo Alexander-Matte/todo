3598566066f909b17e35f9d077a0d44016228f4f
Alex Matte <alex.matte71@gmail.com>
2023-06-15 18:28:30 +0200

    deleted handler file

--- src/handlers.js
+++
@@ -1,6 +0,0 @@
-import { Project } from "./components/Project";
-import { TaskApp } from "./components/TaskApp";
-export function createProject() {
-    let projectName = document.querySelector("#projectName").value;
-    return projectName;
-}
