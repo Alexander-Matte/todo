a2a692d71e2bb2e6bf526eec611dddaca5f946b5
Alex Matte <alex.matte71@gmail.com>
2023-06-20 22:13:45 +0200

    added btn-group to project li. Created functions for rendering edit and delete project buttons

--- src/rendering/renderFuncs.js
+++ src/rendering/renderFuncs.js
@@ -17,10 +17,34 @@ export function renderProjLi (project) {
     let ul = document.querySelector(".project-list");
     let li = document.createElement("li");
     let div = document.createElement("div");
+    let btnDiv = document.createElement("div");
+    btnDiv.classList.add("btn-group")
     div.classList.add("proj-name")
     li.classList.add("proj-li");
     div.innerText = name
+    btnDiv.appendChild(renderProjEditBtn());
+    btnDiv.appendChild(renderProjDeleteBtn());
     li.appendChild(div);
+    li.appendChild(btnDiv)
     ul.appendChild(li);
     return li
+}
+
+function renderProjDeleteBtn () {
+    let btn = document.createElement("button");
+    let icon = document.createElement("icon");
+    btn.classList.add("btn","btn-danger");
+    icon.innerText = "X"
+    btn.appendChild(icon);
+    return btn;
+
+}
+
+function renderProjEditBtn () {
+    let btn = document.createElement("button");
+    let icon = document.createElement("icon");
+    btn.classList.add("btn","btn-dark", "edit-proj", "d-flex");
+    icon.classList.add("fa-regular", "fa-pen-to-square")
+    btn.appendChild(icon);
+    return btn;
 }
