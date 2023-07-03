e16f91c220c9ec9db7a3e6e99f5c245941559925
Alex Matte <alex.matte71@gmail.com>
2023-07-03 21:35:53 +0200

    added task editing logic

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { Todo } from "./components/Todo";
 import { captureTaskInputs, clearContent, getProjectName } from "./helpers";
 import { renderEditInput, renderProjLi, renderTasks } from "./rendering/renderFuncs";
 import { updateContentHeader } from "./helpers";
+import { isEqual, uniq } from 'lodash';
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
@@ -94,51 +95,52 @@ taskForm.addEventListener("submit", function (e) {
         defaultProject.addTodo(task);
     }
     renderTasks(currentProject);
+    captureTaskToEdit(currentProject);
     handleRemoveTodo()
-    handleEditTodo();
     taskForm.reset();
     
 })
 
+let taskToEdit;
 
-//TODO:
-// Build project remove logic --> DONE. Should deleting a project delete all the todos from the "All" project? Look into building
-//  modal that you can choose to delete all todos globally or to keep the todos but delete the project
-// build remove todo logic --> DONE --> When one deletes a Task from a project, should it also delete it from the "All" Project?
-// Build edit todo logic
-// Build edit project logig
-
-// Look into making a template for the content page for when a project doest have any todos. And then maybe a template for when a project does have todos
-    // For example at the top of the page when a project has todos, maybe display "There are no todos in this project, click here to add a todo!"
-// Build a button at top of page that brings a modal to add a todo
-// How to persist data when page is refreshed. Look into how to store locally. Look at TOP instructions
-// create project validation that every project has a unique name
-//
-function handleEditTodo (e) {
-    let editTaskForm = document.querySelector("#task-edit-form");
-    document.querySelector("#modal-edit-close").addEventListener("click", function(e) {
-        editTaskForm.reset();
-    })
-    let editBtns = document.querySelectorAll(".edit-todo");
-    editBtns.forEach(btn => {
-        btn.addEventListener("click", function (e) {
-            let taskName = this.parentElement.parentElement.firstElementChild.textContent;
-            let result = taskApp.currentSelected.tasks.find(item => item.title === taskName);
-            console.log(result)
-        })
-
-    })
-}
-
-let editTaskForm = document.querySelector("#task-edit-form");
-editTaskForm.addEventListener("submit", function(e) {
+let editTaskModal = document.querySelector("#task-edit-form");
+editTaskModal.addEventListener("submit", function(e){
     e.preventDefault();
-    getData(this);
-    this.reset();
+    let currentProject = taskApp.currentSelected;
+    // when i submit, i want to take the new todo information and replace the old todo in the project
+
+    //Capture new todo info
+    let updatedTask = getData(this);
+    if(currentProject.tasks.includes(taskToEdit)) {
+        let i = currentProject.tasks.indexOf(taskToEdit);
+        currentProject.updateTask(i, updatedTask.title, updatedTask.description, updatedTask.dueDate, updatedTask.prio);
+    }
+    clearContent();
+    renderTasks(currentProject);
+    captureTaskToEdit(currentProject);
+    handleRemoveTodo();
+
+    
+    
+
 
 })
 
+function captureTaskToEdit (currentProject) {
+    let editBtns = document.querySelectorAll(".edit-todo");
+    editBtns.forEach(btn => {
+        btn.addEventListener("click", function(e) {
+            let taskTitle = this.parentElement.parentElement.firstElementChild.textContent;
+            let task = currentProject.getTask(taskTitle);
+            taskToEdit = task;
+            document.querySelector("#edit-task-title").value = task.title;
+            document.querySelector("#edit-task-desc").value = task.description;
+            document.querySelector("#edit-task-due").value = task.dueDate;
+            document.querySelector("#edit-task-prio").value = task.prio;
+        });
+    })
 
+}
 
 
 function handleRemoveTodo (e) {
@@ -162,23 +164,27 @@ document.querySelector("#modal-add-close").addEventListener("click", (e) => {
 //Move this to helpers and use for capturing original inputs TODO!!
 function getData(form) {
     let formData = new FormData(form);
-
-    // ...or output as an object
     let result = Object.fromEntries(formData);
-    let updatedTitle = result["task-title"];
-    let updatedDesc = result["task-desc"];
-    let updatedDue = result["task-due"];
-    let updatedPrio = result["task-prio"];
-    console.log(updatedTitle, updatedDesc, updatedDue, updatedPrio);
+    let title = result["task-title"];
+    let description = result["task-desc"];
+    let dueDate = result["task-due"];
+    let prio = result["task-prio"];
+    return {title, description, dueDate, prio};
 
-    // Start here! Update task in project and redisplay the task
     
 };
 
 
+//TODO:
 
 
-
+// Look into making a template for the content page for when a project doest have any todos. And then maybe a template for when a project does have todos
+    // For example at the top of the page when a project has todos, maybe display "There are no todos in this project, click here to add a todo!"
+// How to persist data when page is refreshed. Look into how to store locally. Look at TOP instructions
+// create project validation that every project has a unique name and Task!
+// When one deletes a Task from a project, should it also delete it from the "All" Project? Also if one deletes a project with lots of tasks, should this delete the tasks
+    //from the "All" project?
+// Add CSS to editing project when clicked. maybe make the input box all red and switch the icon out with a save icon
 
 
 
