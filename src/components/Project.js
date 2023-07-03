0818c6d5c16a84b530d89db2083e40d7cd7a6969
Alex Matte <alex.matte71@gmail.com>
2023-07-03 21:34:07 +0200

    added task selection and editing logic

--- src/components/Project.js
+++ src/components/Project.js
@@ -17,4 +17,21 @@ export class Project {
             }
         }
     }
+    
+    getTask (taskTitle) {
+        for (let i = this.tasks.length - 1; i >= 0; --i) {
+            if (this.tasks[i].title === taskTitle) {
+                return this.tasks[i];
+            }
+        }
+    }
+
+    updateTask(index, title,desc,dueDate,prio){
+        this.tasks[index].title = title;
+        this.tasks[index].description = desc;
+        this.tasks[index].dueDate = dueDate;
+        this.tasks[index].prio = prio;
+
+    }
+
 }
